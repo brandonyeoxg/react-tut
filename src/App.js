b14700498c5cde0b2d5314c850a38ad9b26a86a9
brandonyeoxg <brandonyeoxg@gmail.com>
2019-03-09 19:20:33 +0800

    Add props functionality together with destructuring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>My first React app!</h1>
+            <h1>My first React app!</h1>
             <p>Welcome :)</p>
+            <Ninjas name="Ryu" age="25" belt="black" />
+            <Ninjas name="Yoshi" age="35" belt="black" />
       </div>
     );
   }
