79ea678049f5250a18323f75056366ee86f5b557
brandonyeoxg <brandonyeoxg@gmail.com>
2019-03-11 20:45:14 +0800

    Add Axios functionality - Used get method to obtain data from REST API

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rainbow from '../hoc/Rainbow';
+
 const About = () => {
   return (
     <div className="container">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Ninjas from './Ninjas';
 import AddNinjas from "./AddNinja";
+import axios from 'axios';
 
 class Home extends Component {
   state = {
@@ -8,7 +9,8 @@ class Home extends Component {
         {name: 'Ryu', age: 25, belt: 'black', id: 1},
         {name: 'Yoshi', age: 30, belt: 'green', id: 2},
         {name: 'Crystal', age: 20, belt: 'pink', id: 3},
-    ]
+    ],
+    posts: [],
   }
 
   addNinja = (ninja) => {
@@ -29,20 +31,45 @@ class Home extends Component {
   }
 
   componentDidMount() {
-      console.log('Component mounted');
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data.slice(0, 10)
+        });
+      });
   }
 
   componentDidUpdate(prevProps, prevState) {
       console.log('Component updated');
       console.log(prevProps, prevState);
   }
+
   render() {
+
+    const { posts } = this.state;
+
+    const postList = posts.length ? (
+      posts.map(post => {
+        return(
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    );
+
     return(
       <div className="container">
         <h1 className="center">My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
         <AddNinjas addNinja= {this.addNinja}/>
+        {postList}
       </div>
     );
   }
